86a61d5b1c28a7d05a9084b22d6d5aae90e156b8
Vladimir Sibirov <trustmaster@kodigy.com>
2011-12-30 19:14:01 +0400

    Fixed install

--- fbconnect/setup/fbconnect.install.php
+++ fbconnect/setup/fbconnect.install.php
@@ -6,7 +6,7 @@ global $db_users;
 
 if ($db->query("SHOW COLUMNS FROM `$db_users` WHERE `Field` = 'user_fbid'")->rowCount() == 0)
 {
-	cot_extrafield_add('users', 'fbid', 'input', '<input class="text" type="text" maxlength="16" size="16" />', '', '',
+	cot_extrafield_add($db_users, 'fbid', 'input', '<input class="text" type="text" maxlength="16" size="16" />', '', '',
 		false, 'Text', 'FaceBook UID');
 }
 ?>
